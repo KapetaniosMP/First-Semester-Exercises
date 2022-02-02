359b943a0f2095d5c463ace6352e16e25ab0c40d
Michael Panagiotis Kapetanios <92818718+KapetaniosMP@users.noreply.github.com>
2022-02-02 18:56:39 +0200

    Add files via upload

---
+++ ergasia4.py
@@ -0,0 +1,36 @@
+# Convert ASCII value to binary
+def strtobinary(s):
+    ar = []
+    for c in s:
+        ascval = ord(c)
+        binval = bin(ascval)
+        ar.append(binval[2:])
+    return (' '.join(ar))
+with open('textf.txt',mode='r') as text:
+    alpha = ''
+    for alfar in text:
+       alpha += strtobinary(alfar) + ' '
+    words = alpha.split()
+    for i in range(len(words)):
+        if len(words[i]) < 7:
+            b = '0'*(7 - len(words[i])) + words[i]
+            words[i] = b
+    alpha = ''.join(words)
+    print(alpha)
+    rep1 = 0
+    rep2 = 0
+    for bi in ['0','1']:
+        for i in alpha:
+            if i == bi:
+                rep1 += 1
+            else:
+                if rep1 > rep2:
+                    rep2 = rep1
+                rep1 = 0
+        if rep1 > rep2:
+            rep2 = rep1
+        print(rep2,bi)
+        rep2 = 0
+        rep1 = 0
+
+
